738dd1e67c067ca2a82754fddd4e8491f17f9eda
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-10-26 12:29:25 +0100

    Added Destroy

--- Delivery Driver/Assets/Delivery.cs
+++ Delivery Driver/Assets/Delivery.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage;
+
+    private void Start()
+    {
+        Debug.Log(hasPackage);    
+    }
+
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Collision");
@@ -12,14 +23,17 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) 
     {
         
-         if (other.tag == "Package")
+         if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
 
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delivered to customer");
+            hasPackage = false;
         }
 
     }
